43a055d9a7d0435510708864041327f8648abebd
rashmika <rashmika@scaler.com>
2025-12-24 21:17:58 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+VERSION = os.getenv("APP_VERSION", "v1")
+
+@app.route("/")
+def home():
+    return f"Hello from CI/CD + GitOps demo! Version: {VERSION}\n"
+
+@app.route("/healthz")
+def health():
+    return "OK\n", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
